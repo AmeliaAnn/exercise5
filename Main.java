e41a4b9305fcad26abaf36239d8ca1e46bad822e
AmeliaAnn <53219102+AmeliaAnn@users.noreply.github.com>
2019-10-17 08:23:57 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,21 +7,21 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter number: ");
+        System.out.println("Enter number: ");
         int num = in.nextInt();
 
 
-        System.out.print("Table to: " + num + "\n");
-        System.out.print(num + " * 1 = " + num * 1 + "\n");
-        System.out.print(num + " * 2 = " + num * 2 + "\n");
-        System.out.print(num + " * 3 = " + num * 3 + "\n");
-        System.out.print(num + " * 4 = " + num * 4 + "\n");
-        System.out.print(num + " * 5 = " + num * 5 + "\n");
-        System.out.print(num + " * 6 = " + num * 6 + "\n");
-        System.out.print(num + " * 7 = " + num * 7 + "\n");
-        System.out.print(num + " * 8 = " + num * 8 + "\n");
-        System.out.print(num + " * 9 = " + num * 9 + "\n");
-        System.out.print(num + " * 10 = " + num * 10 + "\n");
+        System.out.println("Table to: " + num);
+        System.out.println(num + " * 1 = " + num * 1);
+        System.out.println(num + " * 2 = " + num * 2);
+        System.out.println(num + " * 3 = " + num * 3);
+        System.out.println(num + " * 4 = " + num * 4);
+        System.out.println(num + " * 5 = " + num * 5);
+        System.out.println(num + " * 6 = " + num * 6);
+        System.out.println(num + " * 7 = " + num * 7);
+        System.out.println(num + " * 8 = " + num * 8);
+        System.out.println(num + " * 9 = " + num * 9);
+        System.out.println(num + " * 10 = " + num * 10);
         in.close();
     }
 }
